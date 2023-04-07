16fe0d061d116b598c890f4c1e4b28330369d43c
Shubham Tribhuvan <shubhamtribhuvan8@gmail.com>
2023-04-07 11:49:52 +0530

    nodeadded

--- Nodejs/index.js
+++ Nodejs/index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const cors=require("cors")
-const { shorten } = require("./Function/server");
+const { shorten } = require("./server");
 app.use(express.json())
 app.use(cors())
 
